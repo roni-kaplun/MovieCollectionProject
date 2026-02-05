77c9d736f7723b20ab0e889f3b290aad71cb2e78
Roni <ronikaplun@gmail.com>
2026-02-05 15:46:08 -0600

    Updated Feature page with a few features including buttons and an ability to add a movie to the interested tab.

---
+++ src/components/button/button.tsx
@@ -0,0 +1,9 @@
+interface ButtonProps {
+  label: string;
+  onClick?: () => void;
+}
+
+export default function Button({ label, onClick }: ButtonProps) {
+  return <button onClick={onClick}>{label}</button>;
+}
+
